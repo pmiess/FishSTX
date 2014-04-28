22bcc73bb5e21c3158330c46a3b6e91e3ce97bcf
Philip Miess <Phil_Miess@yahoo.com>
2014-04-28 16:24:19 -0400

    Main Class

---
+++ src/main/java/com/github/pmiess/fishSTX/FishSTX.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.github.pmiess.fishSTX;
+
+/**
+ * @author pmiess
+ *
+ */
+public class FishSTX {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
