1addbf369b64a993326dbba9ca600bed16a1d1de
Philip Miess <Phil_Miess@yahoo.com>
2014-06-23 13:47:51 -0400

    pull in Xalan for XSLTC
    indent
    
    fix UnitTest.xml syntax error

--- src/main/java/com/github/pmiess/fishSTX/FishSTX.java
+++ src/main/java/com/github/pmiess/fishSTX/FishSTX.java
@@ -20,34 +20,34 @@ public class FishSTX {
 	
 public FishSTX(String[] args) throws TransformerException{
 	
-	if(args.length != 3){
-		return;
-	}
-	File input = new File(args[0]);
-	File transform = new File(args[1]);
-	File output = new File(args[2]);
-	if(!(transform.exists() && input.exists() && !output.exists())){
-		return;
-	}
-	
-	
-	// use Joost as transformation engine
-	String key = "javax.xml.transform.TransformerFactory";
-	String valueJOOST = "net.sf.joost.trax.TransformerFactoryImpl";
-	Properties props = System.getProperties();
-	props.put(key, valueJOOST);
-	System.setProperties(props);
-	
-	TransformerFactory factory =  net.sf.joost.trax.TransformerFactoryImpl.newInstance();
-	
-	//use XSLTC to execute XSL transforms
-	factory.setAttribute(net.sf.joost.trax.TrAXConstants.KEY_XSLT_FACTORY, "org.apache.xalan.xsltc.trax.TransformerFactoryImpl");
-	
-	Transformer transformer =
-	factory.newTransformer(new StreamSource(args[1]));
-	
-	transformer.transform(new StreamSource(args[0]), new StreamResult(args[2]));
-}
+		if(args.length != 3){
+			return;
+		}
+		File input = new File(args[0]);
+		File transform = new File(args[1]);
+		File output = new File(args[2]);
+		if(!(transform.exists() && input.exists() && !output.exists())){
+			return;
+		}
+		
+		
+		// use Joost as transformation engine
+		String key = "javax.xml.transform.TransformerFactory";
+		String valueJOOST = "net.sf.joost.trax.TransformerFactoryImpl";
+		Properties props = System.getProperties();
+		props.put(key, valueJOOST);
+		System.setProperties(props);
+		
+		TransformerFactory factory =  net.sf.joost.trax.TransformerFactoryImpl.newInstance();
+		
+		//use XSLTC to execute XSL transforms
+		factory.setAttribute(net.sf.joost.trax.TrAXConstants.KEY_XSLT_FACTORY, "org.apache.xalan.xsltc.trax.TransformerFactoryImpl");
+		
+		Transformer transformer =
+		factory.newTransformer(new StreamSource(args[1]));
+		
+		transformer.transform(new StreamSource(args[0]), new StreamResult(args[2]));
+    }
 
 	/**
 	 * @param args
